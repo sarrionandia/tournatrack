b02dfb89ede1c34e7085f98463c91afdcfebf65b
Roberto Sarrionandia <rbs9@aber.ac.uk>
2014-01-13 21:26:45 +0000

    Reg page framework

---
+++ reg.py
@@ -0,0 +1,55 @@
+# Copyright 2014 Roberto Brian Sarrionandia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import webapp2
+import jinja2
+import os
+
+from google.appengine.ext import ndb
+from google.appengine.api import users
+
+from models import Tournament
+from models import PreRegRecord
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class RegHandler(webapp2.RequestHandler):
+	def get(self):
+		user = users.get_current_user()
+		
+		#Get the requested tournament
+		tid = self.request.get('t')
+		key = ndb.Key('Tournament', int(tid))
+		t = key.get()
+			
+		reg = t.preRegRecord().get()
+					
+		template_values = {
+			'user' : user,
+			't' : t,
+			'logout' : users.create_logout_url('/'),
+			'login' : users.create_login_url('/reg?t=' + tid),
+			'r' : reg,
+		}
+		template = JINJA_ENVIRONMENT.get_template('view/reg.html')
+		self.response.write(template.render(template_values))
+				
+
+
+app = webapp2.WSGIApplication([
+	('/reg', RegHandler)
+], debug=True)
